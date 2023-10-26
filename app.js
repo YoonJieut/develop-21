9857474e128870c0084f39a3f5d3aae69aa59c42
YoonJunHyeon <qmdldos@gmail.com>
2023-10-26 19:59:16 +0900

    getter 기능의 소개 작성, 읽기 전용 함수이다.

--- app.js
+++ app.js
@@ -20,7 +20,15 @@ class User {
     }
   }
 
+  // '가져오는', '읽어오는' 작업을 할 수 있는 getter 기능으로
+  // 읽기 전용 (readonly) 프로퍼티를 정의한다.
+  // getter 함수는 키워드 get을 사용한다.
+  // 실제 값인 _email을 새로 대입하거나 변경하는 목적이 아닌,
+  // 읽어오는 목적으로만 사용한다.
 
+  get email() {
+    return this._email;
+  }
 
 
 }
